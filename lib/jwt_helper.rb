16bfd27abbd896ebd339a0086f059b719336b56f
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2024-12-31 00:05:00 +0530

    fix token auth issue

--- lib/jwt_helper.rb
+++ lib/jwt_helper.rb
@@ -5,16 +5,19 @@ module JwtHelper
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, SECRET_KEY, "HS256")
   end
 
   def self.decode(token)
-    return  nil if token.nil?
+    return nil if token.nil?
 
-    decoded = JWT.decode(token, SECRET_KEY)
-    HashWithIndifferentAccess.new(decoded)
+    decoded = JWT.decode(token, SECRET_KEY, true, algorithm: "HS256")
+    HashWithIndifferentAccess.new(decoded.first)
 
-  rescue JWT::DecodeError
+  rescue JWT::ExpiredSignature
+    raise StandardError, "Token has expired"
+  rescue JWT::DecodeError => e
+    Rails.logger.error("JWT Decode Error: #{e.message}")
     nil
   end
 end
